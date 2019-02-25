312e3d39ed48ebf6700d73fa7fc86dc737a4e43a
songyubin <1970083855@qq.com>
2019-02-25 13:53:17 +0800

    's'

--- pages/about/about.js
+++ pages/about/about.js
@@ -62,9 +62,9 @@ stopluyin(){
 
     wx.showToast({
       title: '成功',  //标题
-      icon: 'loading',  //图标，支持"success"、"loading"
-      image: '../icon/Apple33.png',  //自定义图标的本地路径，image 的优先级高于 icon
-      duration: 2000000, //提示的延迟时间，单位毫秒，默认：1500
+      icon: 'success',  //图标，支持"success"、"loading"
+      // image: '../icon/Apple33.png',  //自定义图标的本地路径，image 的优先级高于 icon
+      duration: 2000, //提示的延迟时间，单位毫秒，默认：1500
       mask: false,  //是否显示透明蒙层，防止触摸穿透，默认：false
       success: function () { }, //接口调用成功的回调函数
       fail: function () { },  //接口调用失败的回调函数
